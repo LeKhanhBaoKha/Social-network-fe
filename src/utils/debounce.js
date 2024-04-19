99389dcf1dd9866055547e35d4ca5037644a7b43
nguyenduyhien123 <93326941+nguyenduyhien123@users.noreply.github.com>
2024-04-19 09:47:00 +0700

    setup project

---
+++ src/utils/debounce.js
@@ -0,0 +1,12 @@
+export function debounce(func, delay) {
+    let timer;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        func.apply(context, args);
+      }, delay);
+    };
+  }
+  
