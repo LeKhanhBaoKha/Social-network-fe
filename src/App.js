5fecfc472745adc18b0562ca0fc7c7d26c2a7104
LeKhanhBaoKha <baokha1024.com@gmail.com>
2024-04-20 21:04:58 +0700

    Them Post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
+import Post from "./components/Post/Post";
 
 function App() {
-  return <SideBar></SideBar>;
+  return (
+    <>
+      <SideBar></SideBar>
+      <Post></Post>
+    </>
+  );
 }
 
 export default App;
