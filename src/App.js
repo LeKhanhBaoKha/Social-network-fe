d4d8a17e4c2d1a35fc2823f26d0d0239d9b5a8f1
nguyenduyhien123 <93326941+nguyenduyhien123@users.noreply.github.com>
2024-05-15 20:21:19 +0700

    add ui navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
 import Post from "./components/Post/Post";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom/dist";
+import { Home } from "./pages/Home";
+import { Layout } from "./layout/Layout";
+import { Components } from "./pages/Conponents";
+import '@/assets/scss/master.scss';
 function App() {
   return (
-    <>
-      <SideBar></SideBar>
-      <Post></Post>
-    </>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="components" element={<Components />}></Route>
+        </Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
