f8760c88f5fb7dc768499434d22093ece0aeadce
nguyenduyhien123 <93326941+nguyenduyhien123@users.noreply.github.com>
2024-05-17 11:46:21 +0700

    add message popup

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom/dist"
 import { Navbar } from "../components/Navbar/Navbar"
+import { PopupMessageDetail } from "../components/Navbar/PopupMessageDetail"
 
 export const Layout = () => {
     return <div className="layout">
         <Navbar></Navbar>
+        <PopupMessageDetail></PopupMessageDetail>
         <Outlet/>
     </div>
-}
+} 
